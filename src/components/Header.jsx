e05457d7bbd2473e9d1ba90a04a3375c1b36b65f
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2025-02-22 19:52:20 +0500

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,11 @@
-import { Select } from "antd";
+import "./header.css";
 
-import vector1 from "../assets/images/vector1.svg";
-import vector2 from "../assets/images/vector2.svg";
-import filter from "../assets/images/filter.svg";
-import { NavLink } from "react-router-dom";
-import uzb from "public/img/flag_uzb.png";
-import eng from "public/img/flag_eng.png";
-import rus from "public/img/flag_rus.png";
-import { useState } from "react";
-import "./styles.css";
-
-const { Option } = Select;
 const Header = () => {
-  const [searchText, setSearchText] = useState("");
-
-  function toggleFullScreen() {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
-  }
-  const handleSearchInput = (e) => {
-    setSearchText(e.target.value);
-  };
-
   return (
     <>
-      <header className="bg-[#2F547EFF] text-[#fff] flex flex-wrap items-center justify-between">
+      <header className="bg-[#2F547EFF] py-2 px-12 text-[#fff] flex flex-wrap items-center justify-between">
         <div className="flex items-center gap-6">
-          <div className="flex gap-4 items-center">
-            <img className="w-[11px] h-[17px]" src={vector1} alt="" />
-            <img className="w-[11px] h-[17px]" src={vector2} alt="" />
-          </div>
-          <div className="nav-search rounded-[100px] border-[2px] border-solid border-[#1E1E1E1A] w-[20rem] p-[0.5rem_1rem] bg-[#F1F2F4] flex items-center justify-between gap-2 text-[#1E1E1E99] font-[500] focus:drop-shadow-[0_15px_15px_rgba(0,0,0,0.2)] hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.15)]">
+          {/* <div className="nav-search rounded-[100px] border-[2px] border-solid border-[#1E1E1E1A] w-[20rem] p-[0.5rem_1rem] bg-[#F1F2F4] flex items-center justify-between gap-2 text-[#1E1E1E99] font-[500] focus:drop-shadow-[0_15px_15px_rgba(0,0,0,0.2)] hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.15)]">
             <i className="fa-solid fa-magnifying-glass w-[1.125rem] h-[1.125rem]"></i>
             <input
               type="text"
@@ -45,53 +17,28 @@ const Header = () => {
             <div className="header_filter relative w-[24px] h-[24px]">
               <img className="w-full h-full" src={filter} alt="" />
             </div>
+          </div> */}
+          <div className="flex items-center focus:drop-shadow-[0_15px_15px_rgba(0,0,0,0.2)] hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.15)] border border-[#EEEEEE] px-3 py-1 bg-[#F5F5F5] flex-1 rounded-[10px]">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Qidiruv"
+              className="w-full outline-none bg-[#F5F5F5] text-[#333] p-[6px_15px]"
+            />
+            <i className="fa-solid fa-magnifying-glass text-[#0054AE]"></i>
           </div>
         </div>
 
-        <nav className="flex items-center gap-6">
-          <NavLink className="text-main_color font-medium" to={"/"}>
-            Home
-          </NavLink>
-          <NavLink className="text-main_color font-medium" to={"/services"}>
-            Services
-          </NavLink>
-          <NavLink className="text-main_color font-medium" to={"/pricing"}>
-            Pricing
-          </NavLink>
-          <div
-            onClick={toggleFullScreen}
-            className="cursor-pointer w-[44px] h-[44px] bg-[#E9F2FF] rounded-full flex items-center justify-center"
-          >
-            <i className="fa-solid fa-expand text-[1.2rem]"></i>
+        <div className="flex items-center gap-2">
+          <div className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-[#fff]">
+          <i className="fa-solid fa-user text-[#0054AE]"></i>
           </div>
-          <Select
-            defaultValue={"uzb"}
-            style={{
-              width: 100,
-            }}
-            onChange={(value) => console.log(`Selected language ${value}`)}
-            placeholder="Select a country"
-          >
-            <Option value="uzb">
-              <div className="flex items-center gap-2">
-                <img src={uzb} alt="USA" style={{ width: 16 }} />
-                UZB
-              </div>
-            </Option>
-            <Option value="rus">
-              <div className="flex items-center gap-2">
-                <img src={rus} alt="Canada" style={{ width: 16 }} />
-                RUS
-              </div>
-            </Option>
-            <Option value="eng">
-              <div className="flex items-center gap-2">
-                <img src={eng} alt="UK" style={{ width: 16 }} />
-                ENG
-              </div>
-            </Option>
-          </Select>
-        </nav>
+          <div>
+            <h2 className="uppercase text-sm">Jamolov X</h2>
+            <p className="text-xs">lavozimi</p>
+          </div>
+        </div>
       </header>
     </>
   );
